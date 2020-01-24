f7d1ce94dbd3b1f4b4861da126d2fdea2cdafb84
lozanofco33 <lozanofco33@gmail.com>
2020-01-24 00:04:16 -0600

    Add files via upload

---
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -0,0 +1,22 @@
+//console.log("Up and running!");
+
+var cards = [
+{
+
+	cardOne : "queen"
+},
+{
+	cardTwo : "queen"
+},
+{
+	cardThree : "king"
+},
+{
+	cardfour : "king"
+},
+
+];
+
+console.log("User flipped " +  cards );
+
+
